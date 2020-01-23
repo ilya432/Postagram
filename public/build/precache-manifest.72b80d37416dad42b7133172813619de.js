fab50b6d5aa40d9bb9011ed63e1df1463be84765
Ilya Levin <ilya432@gmail.com>
2020-01-23 17:35:32 +0200

    after build

---
+++ public/build/precache-manifest.72b80d37416dad42b7133172813619de.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63747a5687c5c6c44b55db5ce09672b0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3bdfc638531e02eb4bc0",
+    "url": "/static/css/main.0c84cf33.chunk.css"
+  },
+  {
+    "revision": "1e0b203dd22a6f0254d0",
+    "url": "/static/js/2.a8cc07d6.chunk.js"
+  },
+  {
+    "revision": "f5c350de243252be974491ba7aa31225",
+    "url": "/static/js/2.a8cc07d6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3bdfc638531e02eb4bc0",
+    "url": "/static/js/main.172c62a1.chunk.js"
+  },
+  {
+    "revision": "c9f1875f7e246a67df7e",
+    "url": "/static/js/runtime-main.cb82405f.js"
+  },
+  {
+    "revision": "92082996e8590f602f9a7ad82eb89bf3",
+    "url": "/static/media/Billabong.92082996.ttf"
+  },
+  {
+    "revision": "75ae2ba25d3d06d5079a940b79114940",
+    "url": "/static/media/loading.75ae2ba2.gif"
+  }
+]);
