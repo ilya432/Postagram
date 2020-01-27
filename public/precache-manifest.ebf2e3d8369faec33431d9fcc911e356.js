61da298f0e1a75847715cfe1ebced1357df10c28
Ilya Levin <ilya432@gmail.com>
2020-01-27 21:14:01 +0200

    after build

---
+++ public/precache-manifest.ebf2e3d8369faec33431d9fcc911e356.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e532e16d7c183cea7395f549f97e772",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a555b1faa3c5a8b371f4",
+    "url": "/static/css/main.6a12d523.chunk.css"
+  },
+  {
+    "revision": "2aef986b29c1d318ab05",
+    "url": "/static/js/2.b50745c5.chunk.js"
+  },
+  {
+    "revision": "f5c350de243252be974491ba7aa31225",
+    "url": "/static/js/2.b50745c5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a555b1faa3c5a8b371f4",
+    "url": "/static/js/main.d34d3e52.chunk.js"
+  },
+  {
+    "revision": "c9f1875f7e246a67df7e",
+    "url": "/static/js/runtime-main.cb82405f.js"
+  },
+  {
+    "revision": "92082996e8590f602f9a7ad82eb89bf3",
+    "url": "/static/media/Billabong.92082996.ttf"
+  },
+  {
+    "revision": "7f690e503a254e0b8349aec0177e07aa",
+    "url": "/static/media/Lato-Regular.7f690e50.ttf"
+  },
+  {
+    "revision": "75ae2ba25d3d06d5079a940b79114940",
+    "url": "/static/media/loading.75ae2ba2.gif"
+  }
+]);
