32da1cee711a5d1950da7646cbe9053a6f71fb8b
Ilya Levin <ilya432@gmail.com>
2020-03-12 21:45:04 +0200

    h

--- services/db.service.js
+++ services/db.service.js
@@ -1,7 +1,7 @@
 
 const MongoClient = require('mongodb').MongoClient;
-const config = require('../config')
-// require('dotenv').config()
+// const config = require('../config')
+require('dotenv').config()
 
 var dbConn = null;
 
@@ -12,11 +12,11 @@ async function getCollection(collectionName) {//returns collection by name
 async function connect() {//returns db by db name
     if (dbConn) return dbConn;
     try {
-        const client = await MongoClient.connect(config.dbURL, { useUnifiedTopology: true });//useNewUrlParser / useUnifiedTopology
+        // const client = await MongoClient.connect(config.dbURL, { useUnifiedTopology: true });//useNewUrlParser / useUnifiedTopology
         // console.log("process.env.REACT_APP_DBURL: " + process.env.REACT_APP_DBURL)
-        // const client = await MongoClient.connect(process.env.REACT_APP_DBURL, { useUnifiedTopology: true });
+        const client = await MongoClient.connect(process.env.REACT_APP_DBURL, { useUnifiedTopology: true });
         const db = client.db(config.dbName);
-        // const db = client.db(process.env.REACT_APP_DBNAME);
+        const db = client.db(process.env.REACT_APP_DBNAME);
         dbConn = db;
         return db;
     } catch (err) {
